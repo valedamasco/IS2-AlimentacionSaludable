9904b534bb867ea97e03b091b4733551d179de92
vdamasco <valedamascoolaizola@gmail.com>
2020-05-14 18:51:40 -0300

    fist commit

--- src/dominio/Sistema.java
+++ src/dominio/Sistema.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import java.util.Currency;
 import javax.swing.ImageIcon;
